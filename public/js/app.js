e1d9aa2eeecd3a7057d11e781472eaef06f4ca30
Derick Bailey <derickbailey@gmail.com>
2012-04-09 09:05:02 -0500

    starting to add marionette based structure to the app

--- public/js/app.js
+++ public/js/app.js
@@ -1,88 +1,96 @@
-$(document).ready(function() {
+var HomeInTouch = (function(exports, Backbone, _, $){
+  var hit = new Backbone.Marionette.Application();
 
-var Model      = Backbone.Model
-  , Collection = Backbone.Collection
-  , View       = Backbone.View
+  var Model      = Backbone.Model
+    , Collection = Backbone.Collection
+    , View       = Backbone.View
 
-  // Models
-  , App
-  , Socket
-  , Addresses
-  , Home
-  , Floor
-  , Room
-  , Group
-  , Device
+    // Models
+    , App
+    , Socket
+    , Addresses
+    , Home
+    , Floor
+    , Room
+    , Group
+    , Device
 
-  // Collections
-  , HomeList
-  , FloorList
-  , RoomList
-  , GroupList
-  , DeviceList
+    // Collections
+    , HomeList
+    , FloorList
+    , RoomList
+    , GroupList
+    , DeviceList
 
-  // Views
-  , AppView
-  , HomeSelector
-  , RoomSelector
-  , FloorView
-  , RoomView
-  , GroupView
-  , LightView
+    // Views
+    , AppView
+    , HomeSelector
+    , RoomSelector
+    , FloorView
+    , RoomView
+    , GroupView
+    , LightView
 
-  , RoomModal
-  , FloorModal
-  , LightModal
+    , RoomModal
+    , FloorModal
+    , LightModal
 
-App = Model.extend({
-  defaults: {
-    connected: false,
-    loaded: false
-  },
+  App = Model.extend({
+    defaults: {
+      connected: false,
+      loaded: false
+    },
 
-  initialize: function() {
-    this.addresses = new Model
-    this.socket = new Socket
+    initialize: function() {
+      this.addresses = new Model
+      this.socket = new Socket
 
-    this.view = new AppView({
-      model: this,
-      el: document.body
-    })
+      this.view = new AppView({
+        model: this,
+        el: document.body
+      })
 
-    this.homes = new HomeList
+      this.homes = new HomeList
 
-    this.on("change:home", function(app, home) {
-      home.floors.view.render()
+      this.on("change:home", function(app, home) {
+        home.floors.view.render()
 
-    }, this)
-  }
-})
+      }, this)
+    }
+  })
 
-AppView = View.extend({})
-HomeList = Collection.extend({})
+  AppView = View.extend({})
+  HomeList = Collection.extend({})
 
-Socket = Model.extend({
-  initialize: function() {
-    var socket = io.connect("http://trendsetterin.selfhost.eu:8081")
+  Socket = Model.extend({
+    initialize: function() {
+      var socket = io.connect("http://trendsetterin.selfhost.eu:8081")
 
-    socket.on("connect", function() {
-      app.set("connected", true)
-    })
+      socket.on("connect", function() {
+        app.set("connected", true)
+      })
 
-    socket.on("disconnect", function() {
-      app.set("connected", false)
-    })
+      socket.on("disconnect", function() {
+        app.set("connected", false)
+      })
 
-    socket.on("homes", function(homes) {
-      app.homes.add(homes)
-      app.set("home", app.homes.models[0])
-    })
+      socket.on("homes", function(homes) {
+        app.homes.add(homes)
+        app.set("home", app.homes.models[0])
+      })
 
-    socket.on("address", function(id, value) {
-      app.addresses.set(id, value)
-    })
-  }
-})
+      socket.on("address", function(id, value) {
+        app.addresses.set(id, value)
+      })
+    }
+  })
 
-window.app = new App
-})
+  // Application Initializer
+  // -----------------------
+
+  hit.addInitializer(function(options){
+    exports.app = new App;
+  });
+
+  return hit;
+})(window, Backbone, _, $);
