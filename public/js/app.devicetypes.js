436c84a495f9d3ada87ac6c44e890045747915ec
Derick Bailey <derickbailey@gmail.com>
2012-04-10 14:37:39 -0500

    comments, and removing some test data from the default home

--- public/js/app.devicetypes.js
+++ public/js/app.devicetypes.js
@@ -2,10 +2,16 @@ HomeInTouch.DeviceTypes = (function(HIT, Backbone, _, $){
   var DeviceTypes = {};
   var deviceTypeCollection = new HIT.DeviceTypeCollection();
 
+  // Public API for device types
+  // ---------------------------
+
+  // Get the DeviceType object by id
   DeviceTypes.get = function(typeId){
     return deviceTypeCollection.get(typeId);
   };
 
+  // Get the name of the device type, by id,
+  // or "(unknown type)" if it's not found.
   DeviceTypes.getName = function(typeId){
     var name;
 
@@ -18,6 +24,9 @@ HomeInTouch.DeviceTypes = (function(HIT, Backbone, _, $){
 
     return name;
   };
+
+  // App event handlers
+  // ------------------
 
   HIT.vent.on("deviceTypes", function(data){
     deviceTypeCollection.reset(data);
