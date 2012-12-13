833058649e84e1b28fdcba0f80a11b08c41539b0
Julian Alessandro <melkor668@gmail.com>
2012-12-13 00:53:13 -0300

    #145: API added to handle SVG images in server.

--- app/controllers/images.js
+++ app/controllers/images.js
@@ -9,14 +9,26 @@ exports.show = function(req, res, next) {
 };
 
 exports.create = function(req, res) {
-	
-	images.put(new Buffer(req.body.fileStream), function(err, doc) {
-    	res.writeHead(200, { 'Content-Type': 'application/json' }); 
-  		res.end(JSON.stringify({
-      		success: true,
-      		imagePath: 'api/images/' + doc._id
-  		}));
-	});
+  	images.put(new Buffer(req.body.fileStream), function(err, doc) {
+      	res.writeHead(200, { 'Content-Type': 'application/json' }); 
+    		res.end(JSON.stringify({
+        		success: true,
+        		imagePath: 'api/images/' + doc._id
+    		}));
+  	});
+};
 
-	
+exports.svgGet = function(req, res, next) {
+    console.log('Image path: ' + req.params.image);
+
+    var imagePath = req.params.image.replace(/\./g, '/');
+    imagePath = './public/img/svg/' + imagePath + '.svg';
+
+    console.log('Full Image Path: ' + imagePath);
+
+    fs.readFile(imagePath, function(err, data) {
+        if (err) return next(err);
+        var result = data.toString().replace(/#000000/g, req.query.color);
+        res.end(result, 'binary');
+    });
 }
