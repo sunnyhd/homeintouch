578cf60e3ccadf4e912af5160b895587d60bd3f1
Martin Moscovich <martinmoscovich@gmail.com>
2013-04-23 18:33:19 -0300

    Added player tabs view

---
+++ client/app/views/players2/player_tabs_item.js
@@ -0,0 +1,22 @@
+
+
+module.exports = Backbone.Marionette.ItemView.extend({
+
+    tagName: 'li',
+
+    template: require('templates/players/player_tabs_item'),
+
+    events: {   
+        'click a': 'playerClicked'
+    },
+
+    onRender: function() {
+        this.$el.attr('data-playerid', this.model.id);
+    },
+
+    playerClicked: function(e) {
+        e.preventDefault();
+        //playersController.activatePlayer(this.model);
+    }
+
+});
