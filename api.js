0267944cb626ad7fc02edcd7ba37049fdb3f725e
Martin Moscovich <martinmoscovich@gmail.com>
2013-06-26 10:42:16 -0300

    refactored scrapper

--- api.js
+++ api.js
@@ -1,9 +1,19 @@
 var lastfm = require('./lib/scrappers/lastfm_music_scapper');
 
+lastfm.getArtistImage('aerosmith')
+    .then(function(url) {
+        console.log(url);  
+    })
+    .fail(function(err) {
+         console.log(err);
+    })
+    .done();
+
 lastfm.getAlbumImage('aerosmith', 'O, Yeah! Ultimate Aerosmith Hits')
     .then(function(url) {
         console.log(url);  
     })
     .fail(function(err) {
          console.log(err);
-    });
+    })
+    .done();
