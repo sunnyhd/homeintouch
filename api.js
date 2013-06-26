023d6d6956c66a76a09b5fd97f5bf3c44aa78cbf
Martin Moscovich <martinmoscovich@gmail.com>
2013-06-26 01:48:43 -0300

    added lastfm music scrapper

---
+++ api.js
@@ -0,0 +1,9 @@
+var lastfm = require('./lib/scrappers/lastfm_music_scapper');
+
+lastfm.getAlbumImage('aerosmith', 'O, Yeah! Ultimate Aerosmith Hits')
+    .then(function(url) {
+        console.log(url);  
+    })
+    .fail(function(err) {
+         console.log(err);
+    });
